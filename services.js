3cb97ea523a1c1a399a198cd640df813331266ec
Roman Sydorchuk <roman.s@ikrok.net>
2023-05-02 18:05:46 +0300

    feat(services): added token check functionality before fetch

--- services.js
+++ services.js
@@ -1,8 +1,26 @@
-export async function getAttachments() {
-    return fetch("./api/fakeapi.json", {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
+const NO_TOKEN = 'NO_TOKEN';
+const getTokenFromURL = () =>{
+    const hash = window.location.hash;
+    if (hash !== '') { // if token is in path
+        return hash.slice(1);
+    } else{ // if token is in parameter
+        if (window.location.search) {
+            return window.location.search.slice(3);
+        } else {
+            return NO_TOKEN;
         }
-    }).then((data) => data.json());
+    }
+}
+
+export async function getAttachments() {
+    // if (getTokenFromURL() === NO_TOKEN) {
+    //     return {code: 2};
+    // } else {
+        return fetch("./api/fakeapi.json", {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then((data) => data.json());
+    // }
 }
