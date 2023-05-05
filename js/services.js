5c66404854f6582dd7824bff3281b171d115ffbd
Roman Sydorchuk <roman.s@ikrok.net>
2023-05-05 11:08:53 +0300

    refactor(services): removed unnecessary consol log

--- js/services.js
+++ js/services.js
@@ -56,7 +56,6 @@ export async function getAttachments() {
             });
 
         if (!response.ok) {
-            console.log('not ok', responseStatusValidator(response));
             return responseStatusValidator(response);
         }
 
